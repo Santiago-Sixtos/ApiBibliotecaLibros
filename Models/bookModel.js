a27c505d9188d80a31bb36aa770145d9e96e2987
Santiago Sixtos <menny_2208@hotmail.com>
2024-12-02 09:33:48 -0600

    Modificaciones a bookModel

--- Models/bookModel.js
+++ Models/bookModel.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'books.json');
 
-const getBooks = () => {
-    const data = fs.readFileSync(filePath);
+const getBooks = async () => {
+    const data = await fs.readFileSync(filePath);
     return JSON.parse(data);
 };
 
-const saveBooks = (books) => {
+const saveBooks = async (books) => {
     fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
 };
 
